bdc115546c033d1d8dd7772f02c0060e2ef548ef
kaliaaa1 <121272250+kaliaaa1@users.noreply.github.com>
2023-03-04 23:10:35 +0800

    Update bettercap.sh

--- bettercap.sh
+++ bettercap.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 echo"\033[43;35m 自动化安装bettercap \033[0m"
-pkg update && pkg upgrade -y
+pkg update && pkg -y upgrade
 pkg install golang git libpcap libusb root-repo tsu -y
 pkg install libnetfilter-queue -y
 pkg install make && pkg install make-guile -y
