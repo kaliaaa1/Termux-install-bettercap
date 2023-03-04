f8638e0a38f928b9ea3e3a0b7fe77e70e363d9d2
kaliaaa1 <121272250+kaliaaa1@users.noreply.github.com>
2023-03-04 22:40:01 +0800

    Update bettercap.sh

--- bettercap.sh
+++ bettercap.sh
@@ -3,6 +3,7 @@ echo"\033[43;35m 自动化安装bettercap \033[0m"
 pkg install update && pkg install upgrade
 pkg install golang git libpcap libusb root-repo tsu
 pkg install libnetfilter-queue
+pkg install make && pkg install make-guile
 git clone https://github.com/bettercap/bettercap.git
 cd bettercap
 make build
