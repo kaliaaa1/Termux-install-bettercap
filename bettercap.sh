21668f7fe1188771fbaddfc63153c7437bab6183
kaliaaa1 <121272250+kaliaaa1@users.noreply.github.com>
2023-03-05 10:42:40 +0800

    Add files via upload

--- bettercap.sh
+++ bettercap.sh
@@ -1,19 +1,26 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo"自动化安装bettercap"
-pkg update && pkg -y upgrade
+echo "自动化安装bettercap"
+pkg update && pkg upgrade -y
 pkg install golang git libpcap libusb root-repo tsu -y
 pkg install libnetfilter-queue -y
-pkg install make && pkg install make-guile -y
-pkg install pkg-config
-echo"依赖安装完成"
+pkg install -y make make-guile
+pkg install pkg-config -y
+echo "依赖安装完成"
+if [ -d "/data/data/com.termux/files/bettercap" ]; then
+    cd bettercap
+    make build
+    sudo make install
+    sudo bttercap
+else 
 git clone https://github.com/bettercap/bettercap.git
 cd bettercap
 make build
 sudo make install
-echo"                  "
-echo"                  "
-echo"安装完成"
-echo"                  "
-echo"                  "
+echo "                  "
+echo "                  "
+echo "安装完成"
+echo "                  "
+echo "                  "
 sleep 5s
 sudo bettercap
+fi
