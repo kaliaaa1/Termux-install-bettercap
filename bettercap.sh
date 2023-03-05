7e3b4afd2d431d25a9470122079d537c3dc5c780
kaliaaa1 <121272250+kaliaaa1@users.noreply.github.com>
2023-03-05 12:34:28 +0800

    Update bettercap.sh

--- bettercap.sh
+++ bettercap.sh
@@ -16,9 +16,11 @@ git clone https://github.com/bettercap/bettercap.git
 cd bettercap
 make build
 sudo make install
-sudo mount -o rw,remount /
-sudo mkdir -p /home/builder/.termux-build/_cache/18-arm-21-v2/bin/
-sudo ln -s `which pkg-config` /home/builder/.termux-build/_cache/18-arm-21-v2/bin/arm-linux-androideabi-pkg-config
+if [! -d "/home"]; then
+    sudo mount -o rw,remount /
+    sudo mkdir -p /home/builder/.termux-build/_cache/18-arm-21-v2/bin/
+    sudo ln -s `which pkg-config` /home/builder/.termux-build/_cache/18-arm-21-v2/bin/arm-linux-androideabi-pkg-config
+else
 echo "bug修复完毕准备启动bettercap"
 sleep 5s
 sudo bettercap
