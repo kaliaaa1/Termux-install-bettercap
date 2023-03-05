af3db2485b039310cdef5a1bc019fcbbf74af822
kaliaaa1 <121272250+kaliaaa1@users.noreply.github.com>
2023-03-05 12:47:24 +0800

    Update bettercap.sh

--- bettercap.sh
+++ bettercap.sh
@@ -1,11 +1,11 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo "自动化安装bettercap"
+echo -e "\033[31m 自动化安装bettercap \033[0m"
 pkg update && pkg upgrade -y
 pkg install golang git libpcap libusb root-repo tsu -y
 pkg install libnetfilter-queue -y
 pkg install make && pkg install make-guile -y
 pkg install pkg-config -y
-echo "依赖安装完成"
+echo -e "\033[31m 依赖安装完成 \033[0m"
 if [ -d "/data/data/com.termux/files/home/Termux-install-bettercap/bettercap" ]; then
     cd bettercap
     make build
@@ -21,7 +21,7 @@ if [! -d "/home"]; then
     sudo mkdir -p /home/builder/.termux-build/_cache/18-arm-21-v2/bin/
     sudo ln -s `which pkg-config` /home/builder/.termux-build/_cache/18-arm-21-v2/bin/arm-linux-androideabi-pkg-config
 else
-echo "bug修复完毕准备启动bettercap"
+echo -e "\033[31m bug修复完毕准备启动bettercap \033[0m"
 sleep 5s
 sudo bettercap
 fi
